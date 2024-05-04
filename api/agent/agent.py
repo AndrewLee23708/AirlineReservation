1f6d00903f78ef60b1e29a7ddd8e0b6997458499
Andrew Lee <andrewlee23708@gmail.com>
2024-05-04 12:32:03 +0800

    working on agent  iteration

--- api/agent/agent.py
+++ api/agent/agent.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template, request, session, url_for, redirect, Blueprint, flash     #make sure to import from flask
 from flask_bcrypt import Bcrypt
+import forms
+from io import BytesIO
+import matplotlib
+matplotlib.use('Agg')  # Runtime error solution
+from matplotlib.ticker import MaxNLocator
+import matplotlib.pyplot as plt
+import base64
+from database import setup_db
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-from ..database import setup_db
+agent = Blueprint('agent',__name__)
 
 # viewing flights booked for customers
-@agent.route('/purchased_flights', methods = ['GET', 'POST'])
-def purchased_flights():
+# Defaults will be showing all the upcoming flights operated by the airline he/she works for
+# the next 30 days. He/she will be able to see all the current/future/past flights operated by the airline he/she
+# works for based range of dates, source/destination airports/city etc. He/she will be able to see all the
+# customers of a particular flight.
+@agent.route('/agent_profile', methods = ['GET', 'POST'])
+def agent_profile():
     conn = setup_db()
     cur = conn.cursor(dictionary=True)
+
     #query
     query = f"SELECT * FROM booking_agent NATURAL JOIN ticket WHERE booking_agent_id = '{session['agent_id']}'"
     cur.execute(query)
@@ -15,181 +30,86 @@ def purchased_flights():
     cur.close()
     print(purchased_for)
     
-    return render_template('purchased_flights.html', purchased_for = purchased_for)
-
-# searching for upcoming flights for customers
-@agent.route('/upcoming_flights/<search_result>', methods = ['GET'])
-def upcoming_flights(search_result):
-    search_result = json.loads(search_result)
-
-    return render_template('upcoming_flights.html', search_result = search_result)
-
-# purchasing tickets for customers
-@agent.route('/purchasing_tickets_for/<flight_num>', methods = ['GET', 'POST'])
-def purchasing_tickets_for(flight_num):
-    print(form)
-    # first get flight information
-    cur = conn.cursor(dictionary=True)    
-    query = f"SELECT * FROM ticket NATURAL JOIN flight WHERE flight_num = '{flight_num}'"
-    cur.execute(query)
-    flight = cur.fetchone()
-    cur.close()
-
-    # second get seat info for that flight
-    cur = conn.cursor(dictionary=True)
-    query = f"SELECT count(ticket_id) as remaining_seats FROM ticket NATURAL JOIN flight WHERE flight_num = '{flight_num}' AND NOT EXISTS (SELECT * FROM purchases WHERE purchases.ticket_id = ticket.ticket_id)"
-    cur.execute(query)
-    remaining_seats = cur.fetchone()
-    cur.close()
-
-    # show all num of tickets by flight (including purchased) DO WE NEED THIS?
-    cur = conn.cursor(dictionary=True)
-    query = f"SELECT count(ticket_id) as all FROM ticket NATURAL JOIN flight WHERE flight_num = '{flight_num}' GROUP BY flight_num;"
-    cur.execute(query)
-    all = cur.fetchone()
-    cur.close()
-
-    # if there are still seats, we need to update seat information
-    if form.validate_on_submit():
-        cur = conn.cursor(dictionary=True)  
-        query = f"SELECT count(ticket_id) as seat_total FROM ticket NATURAL JOIN flight where flight_num = '{flight_num}' and NOT EXISTS(SELECT * FROM purchases WHERE purchases.ticket_id = ticket.ticket_id)"
-        cur.execute(query)
-        seat_total = cur.fetchone()
-        cur.close()
-        
-        # this is the newly updated seat info
-        cur = conn.cursor(dictionary=True)  
-        query = f"SELECT count(ticket_id) as all2 FROM ticket NATURAL JOIN flight WHERE flight_num = '{flight_num}' GROUP BY flight_num"
-        cur.execute(query)
-        all2 = cur.fetchone()
-        cur.close()
-        print("Updated")
-
-        # update customer and agent instances from this session
-        customer = form.customer.data
-        form.agent.data = agent = session['agent_id']
-
-        # displaying all available flights
-        cur = conn.cursor(dictionary=True)  
-        query_available = "SELECT * FROM ticket WHERE flight_num = '{}' AND NOT EXISTS (SELECT * FROM purchases WHERE purchases.ticket_id = ticket.ticket_id)"
-        cur.execute(query_available.format(flight_num))
-        result = cur.fetchall()
-
-        # successfully purchased ticket UI result
-        if len(result) == 0:
-            flash("Ticket not available", "Danger")
-            cur.close()
-        else:
-            airline_name = result[0]["airline_name"]
-            flight_num = result[1]["flight_num"]
-            ticket_id = result[2]["ticket_id"]
-
-            # grab agent
-            cur = conn.cursor(dictionary=True)  
-            query_agent_airline = "SELECT * FROM booking_Agent NATURAL JOIN ba_works_For WHERE booking_agent_id = '{}' AND airline_name = '{}'"
-            cur.execute(query_agent_airline.format(agent, airline_name))
-            works_for = cur.fetchall()
-            print(works_for)
-
-            # do I need to verify customer?
-            cur = conn.cursor(dictionary=True)  
-            query_find_customer = "SELECT * FROM customer WHERE email = '{}'"
-            cur.execute(query_find_customer.format(customer))
-            found_customer = cur.fetchall()
-
-            if agent is not None and len(works_for) == 0:
-                flash("You are not a verified Booking Agent for this airline.", "Danger")
-            else:
-                if len(found_customer) == 0:
-                    flash("Customer ID not found.", "Danger")
-                else:
-                    date = datetime.date.today()
-                    query = f"INSERT INTO purchases VALUES ('{ticket_id}', '{flight_num}', '{customer_email}', '{booking_agent_email}', '{date}')"
-                    cur.execute(query)
-                    conn.commit()
-                    flash(f'User {session["email"]} purchased ticket No. {ticket_id} from {flight_num} at {airline_name} through booking agent {booking_agent_email}.', 'Success')
-                    cur.close()
-                    return redirect(url_for('home'))
-    return render_template("purchasing_tickets_for.html", form = form, flight = flight, remaining_seats = remaining_seats, all2 = all2)
+    return render_template('agent_profile.html', purchased_for = purchased_for)
 
 # view agent's commissions
-@agent.route('/view_commissions', methods = ['GET', 'POST'])
+@agent.route('/agent_view_commissions', methods = ['GET', 'POST'])
 def view_commissions():
-    form_view_commissions = forms.ViewCommissionsForm()
+#     form_view_commissions = forms.ViewCommissionsForm()
     
-    # getting dates
-    if form_view_commissions.start.data == None:
-        start = datetime.date.today() - relativedelta(years = 1)
-    else:
-        start = form_view_commissions.start.data
+#     # getting dates
+#     if form_view_commissions.start.data == None:
+#         start = datetime.date.today() - relativedelta(years = 1)
+#     else:
+#         start = form_view_commissions.start.data
 
-    if form_view_commissions.end.data == None:
-        end = datetime.date.today()
-    else:
-        end = form_view_commissions.end.data
+#     if form_view_commissions.end.data == None:
+#         end = datetime.date.today()
+#     else:
+#         end = form_view_commissions.end.data
 
-    # total amount of commission 
-    cur = conn.cursor(dictionary=True)  
-    query = f"SELECT SUM(price)*0.1 AS commissions_sum FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']} AND purchase_date >= '{start_date}' AND purchase_date <= '{end_date}'"
-    cur.execute(query)
-    sum_commissions = cur.fetchone()['commmissions_sum']
-    cur.close()
+#     # total amount of commission 
+#     cur = conn.cursor(dictionary=True)  
+#     query = f"SELECT SUM(price)*0.1 AS commissions_sum FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']} AND purchase_date >= '{start_date}' AND purchase_date <= '{end_date}'"
+#     cur.execute(query)
+#     sum_commissions = cur.fetchone()['commmissions_sum']
+#     cur.close()
     
-    # total number of commissions
-    cur = conn.cursor(dictionary=True)  
-    query = f"SELECT COUNT(*) AS ticket_count FROM purchases NATURAL JOIN ticket WHERE booking_agent_id = '{session['agent_id']}' AND purchase_date >= '{start_date}' AND purchase_date <= '{end_date}'"
-    cur.execute(query)
-    num_tickets = cur.fetchone()['ticket_count']
-    cur.close()
+#     # total number of commissions
+#     cur = conn.cursor(dictionary=True)  
+#     query = f"SELECT COUNT(*) AS ticket_count FROM purchases NATURAL JOIN ticket WHERE booking_agent_id = '{session['agent_id']}' AND purchase_date >= '{start_date}' AND purchase_date <= '{end_date}'"
+#     cur.execute(query)
+#     num_tickets = cur.fetchone()['ticket_count']
+#     cur.close()
 
-    # avg commission
-    if commissions_sum == 0:
-        print("No commissions")
-    else:
-        commissions_avg = float(sum_commissions)/float(num_tickets)
+#     # avg commission
+#     if commissions_sum == 0:
+#         print("No commissions")
+#     else:
+#         commissions_avg = float(sum_commissions)/float(num_tickets)
     
-    print(num_tickets, sum_commissions, commissions_avg)
-    return render_template('view_comissions.html', form_view_commissions = form_view_commissions, num_tickets = num_tickets, sum_commissions = sum_commissions, commissions_avg = commissions_avg)
+    # print(num_tickets, sum_commissions, commissions_avg)
+    return render_template('agent_view_comissions.html') #form_view_commissions = form_view_commissions, num_tickets = num_tickets, sum_commissions = sum_commissions, commissions_avg = commissions_avg)
 
 # view top customers
 @agent.route('/view_top_customers', methods = ['GET', 'POST'])
 def view_top_customers():
-    # top 5: # of tickets purchased in last 6 months from this booking agent
-    cur = conn.cursor(dictionary=True)  
-    query - f"SELECT customer_email, COUNT(*) AS ticket_count FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']} AND purchase_date >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH) GROUP BY customer_email ORDER BY ticket_count DESC LIMIT 5"
-    cur.execute(query)
-    top_5_ticket_count = cur.fetchall()
-    cur.close()
+#     # top 5: # of tickets purchased in last 6 months from this booking agent
+#     cur = conn.cursor(dictionary=True)  
+#     query - f"SELECT customer_email, COUNT(*) AS ticket_count FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']} AND purchase_date >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH) GROUP BY customer_email ORDER BY ticket_count DESC LIMIT 5"
+#     cur.execute(query)
+#     top_5_ticket_count = cur.fetchall()
+#     cur.close()
 
-    temp = {'customer_email': [], 'ticket_count': []}
-    for i in range(5):
-        if i < len(top_5_ticket_count):
-            customer = top_5_ticket_count[i]
-            temp['customer_email'].append(customer['customer_email'])
-            temp['ticket_count'].append(customer['ticket_count'])
-        else:
-            temp['customer_email'].append("Null")
-            temp['ticket_count'].append(0)
-    top_5_ticket_count = temp
-    print(top_5_ticket_count)
+#     temp = {'customer_email': [], 'ticket_count': []}
+#     for i in range(5):
+#         if i < len(top_5_ticket_count):
+#             customer = top_5_ticket_count[i]
+#             temp['customer_email'].append(customer['customer_email'])
+#             temp['ticket_count'].append(customer['ticket_count'])
+#         else:
+#             temp['customer_email'].append("Null")
+#             temp['ticket_count'].append(0)
+#     top_5_ticket_count = temp
+#     print(top_5_ticket_count)
 
-    # top 5: amount of commission received in last yr
-    cur = conn.cursor(dictionary=True)  
-    query = f"SELECT custoemr_email, SUM(price)*0.1 AS commission_amt FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']}' AND purchase_date >= DATE_SUB(CURDATE(), INTERVAL 12 MONTHS) GROUP BY customer_email ORDER BY commission_amt DESC LIMIT 5"
-    cur.execute(query)
-    top_5_commission_amt = cur.fetchall()
-    cur.close()
+#     # top 5: amount of commission received in last yr
+#     cur = conn.cursor(dictionary=True)  
+#     query = f"SELECT custoemr_email, SUM(price)*0.1 AS commission_amt FROM purchases NATURAL JOIN ticket NATURAL JOIN flight WHERE booking_agent_id = '{session['agent_id']}' AND purchase_date >= DATE_SUB(CURDATE(), INTERVAL 12 MONTHS) GROUP BY customer_email ORDER BY commission_amt DESC LIMIT 5"
+#     cur.execute(query)
+#     top_5_commission_amt = cur.fetchall()
+#     cur.close()
 
-    temp = {'customer_email': [], 'commission_amt': []}
-    for i in range(5):
-        if i < len(top_5_commission_amt):
-            customer = top_5_commission_amt[i]
-            temp['customer_email'].append(customer['customer_email'])
-            temp['commission_amt'].append(customer['commission_amt'])
-        else:
-            temp['customer_email'].append("Null")
-            temp['commission_amt'].append(0)
-        top_5_commission_amt = temp
-        print(top_5_commission_amt)
+#     temp = {'customer_email': [], 'commission_amt': []}
+#     for i in range(5):
+#         if i < len(top_5_commission_amt):
+#             customer = top_5_commission_amt[i]
+#             temp['customer_email'].append(customer['customer_email'])
+#             temp['commission_amt'].append(customer['commission_amt'])
+#         else:
+#             temp['customer_email'].append("Null")
+#             temp['commission_amt'].append(0)
+#         top_5_commission_amt = temp
+#         print(top_5_commission_amt)
 
-    return render_template('view_top_customers.html', top_5_ticket_count = json.dumps(top_5_ticket_count), top_5_commission_amt = json.dumps(top_5_commission_amt))
+    return render_template('agent_view_customers.html') #top_5_ticket_count = json.dumps(top_5_ticket_count), top_5_commission_amt = json.dumps(top_5_commission_amt))
--- api/staff/staff.py
+++ api/staff/staff.py
@@ -68,20 +68,37 @@ def staff_profile():
 
 # ### Admin only tool box, contains all functionalities for admin
 # Includes 5 functionalities
+# Create flight, insert one-by-one
 @staff.route('/staff_create_flight', methods=['GET', 'POST'])
 def staff_create_flight():
 
     form = forms.CreateFlight()
     if form.validate_on_submit():
+        conn = setup_db()
+        cur = conn.cursor(dictionary=True)
+
+        # Check if the flight number already exists
+        cur.execute("SELECT * FROM flight WHERE flight_num = %s AND airline_name = %s", (form.flight_num.data, session['airline']))
+        if cur.fetchone():
+            flash('The flight already exists!', 'danger')
+            return render_template('staff_create_flight.html', form=form)
+
+        # Check if the airplane exists and get the number of seats
+        cur.execute("SELECT seats FROM airplane WHERE airplane_id = %s AND airline_name = %s", (form.airplane_id.data, session['airline']))
+        airplane = cur.fetchone()
+        if not airplane:
+            flash('Airplane does not exist!', 'danger')
+            return render_template('staff_create_flight.html', form=form)
+
         try:
-            conn = setup_db()
-            cur = conn.cursor()
+            # Add the new flight
             query = """
-                INSERT INTO flight (flight_num, departure_airport, departure_time, arrival_airport, arrival_time, price, status, airplane_id)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO flight (airline_name, flight_num, departure_airport, departure_time, arrival_airport, arrival_time, price, status, airplane_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
-            cur.execute(query, (form.flight_num.data, form.departure_airport.data, form.departure_time.data, form.arrival_airport.data, form.arrival_time.data, form.price.data, form.status.data, form.airplane_id.data))
+            cur.execute(query, (session['airline'], form.flight_num.data, form.departure_airport.data, form.departure_time.data, form.arrival_airport.data, form.arrival_time.data, form.price.data, form.status.data, form.airplane_id.data))
             conn.commit()
+
             flash('Flight created successfully!', 'success')
         except Exception as e:
             flash(f'Failed to create flight. Error: {str(e)}', 'danger')
@@ -89,45 +106,61 @@ def staff_create_flight():
             cur.close()
     return render_template('staff_create_flight.html', form=form)
 
+
 ### Looks good
 ## Add airplane
 @staff.route('/staff_add_airplane', methods=['GET', 'POST'])
 def staff_add_airplane():
-
     form = forms.AddAirplane()
     if form.validate_on_submit():
+        conn = setup_db()
+        cur = conn.cursor(dictionary=True)
+
+        # Check if the airplane already exists in the database
+        cur.execute("SELECT * FROM airplane WHERE airplane_id = %s AND airline_name = %s", (form.airplane_id.data, session['airline']))
+        if cur.fetchone():
+            flash('This airplane already exists!', 'danger')
+            return render_template('staff_add_airplane.html', form=form)
+
         try:
-            conn = setup_db()
-            cur = conn.cursor()
+            # Insert the new airplane
             query = "INSERT INTO airplane (airplane_id, seats, airline_name) VALUES (%s, %s, %s)"
             cur.execute(query, (form.airplane_id.data, form.seats.data, session['airline']))
             conn.commit()
             flash('Airplane added successfully!', 'success')
-
         except Exception as e:
             flash(f'Failed to add airplane. Error: {str(e)}', 'danger')
-
         finally:
             cur.close()
+
     return render_template('staff_add_airplane.html', form=form)
 
 
 @staff.route('/staff_add_airport', methods=['GET', 'POST'])
 def staff_add_airport():
-
     form = forms.AddAirport()
     if form.validate_on_submit():
-        try:
-            conn = setup_db()
-            cur = conn.cursor()
-            query = "INSERT INTO airport (airport_name, airport_city) VALUES (%s, %s)"
-            cur.execute(query, (form.airport_name.data, form.airport_city.data))
-            conn.commit()
-            flash('Airport added successfully!', 'success')
-        except Exception as e:
-            flash(f'Failed to add airport. Error: {str(e)}', 'danger')
-        finally:
-            cur.close()
+        conn = setup_db()
+        cur = conn.cursor(dictionary=True)
+
+        # Check if  airport already exists
+        cur.execute("SELECT * FROM airport WHERE airport_name = %s", (form.airport_name.data,))
+        existing_airport = cur.fetchone()
+        
+        if existing_airport:
+            flash('This airport already exists!', 'danger')
+        else:
+            try:
+                # Insert the new airport
+                query = "INSERT INTO airport (airport_name, airport_city) VALUES (%s, %s)"
+                cur.execute(query, (form.airport_name.data, form.airport_city.data))
+                conn.commit()
+                flash('Airport added successfully!', 'success')
+            except Exception as e:
+                flash(f'Failed to add airport. Error: {str(e)}', 'danger')
+            finally:
+                cur.close()
+                
     return render_template('staff_add_airport.html', form=form)
 
 ### Make sure to add constraint that staff has to be in the same airline as staff being changed.
@@ -136,38 +169,37 @@ def staff_grant_permission():
     form = forms.GrantPermissions()
     if form.validate_on_submit():
         conn = setup_db()
-        cur = conn.cursor()
+        cur = conn.cursor(dictionary=True)
 
         try:
-            # Fetch the airline name of the logged-in staff
-            cur.execute("SELECT airline_name FROM airline_staff WHERE username = %s", (session['username'],))
-            staff_airline = cur.fetchone()
+            # Fetch the airline name of the staff being updated
+            cur.execute("SELECT airline_name FROM airline_staff WHERE username = %s", (form.username.data,))
+            target_airline = cur.fetchone()
             
-            if staff_airline:
-                # Fetch the airline name of the staff being updated
-                cur.execute("SELECT airline_name FROM airline_staff WHERE username = %s", (form.username.data,))
-                target_airline = cur.fetchone()
-                
-                if target_airline and staff_airline['airline_name'] == target_airline['airline_name']:
-                    # Perform update if they are in the same airline
-                    query = "INSERT INTO permission (username, permission_type) VALUES (%s, %s) ON DUPLICATE KEY UPDATE permission_type = VALUES(permission_type)"
-                    cur.execute(query, (form.username.data, form.permission.data))
-                    conn.commit()
-                    flash('Permission granted successfully!', 'success')
-                else:
-                    flash('Cannot update staff from different airline.', 'danger')
+            # Check if the staff being updated is in the same airline
+            if target_airline and target_airline['airline_name'] == session.get('airline'):
+                # Perform update if they are in the same airline
+                query = """
+                    INSERT INTO permission (username, permission_type)
+                    VALUES (%s, %s)
+                    ON DUPLICATE KEY UPDATE permission_type = VALUES(permission_type)
+                """
+                cur.execute(query, (form.username.data, form.permission.data))
+                conn.commit()
+                flash('Permission granted successfully!', 'success')
             else:
-                flash('Your airline information could not be verified.', 'danger')
+                flash('Cannot update staff from different airline.', 'danger')
                 
         except Exception as e:
             flash(f'Failed to grant permission. Error: {str(e)}', 'danger')
         finally:
             cur.close()
+
     return render_template('staff_grant_permission.html', form=form)
 
 
-
 ### Make sure to add constraint that staff has to be in the same airline as agent being changed.
+# Works
 @staff.route('/staff_add_agent', methods=['GET', 'POST'])
 def staff_add_agent():
     form = forms.AddAgent()
@@ -176,20 +208,16 @@ def staff_add_agent():
         cur = conn.cursor()
 
         try:
-            # Verify that the airline name matches the logged-in staff's airline
-            cur.execute("SELECT airline_name FROM airline_staff WHERE username = %s", (session['username'],))
-            staff_airline = cur.fetchone()
+            # Use staff's airline from the session
+            airline_name = session.get('airline')
+            query = "INSERT INTO booking_agent_work_for (email, airline_name) VALUES (%s, %s)"
+            cur.execute(query, (form.email.data, airline_name))
+            conn.commit()
+            flash('Booking agent added successfully to the airline!', 'success')
 
-            if staff_airline and staff_airline['airline_name'] == form.airline_name.data:
-                query = "INSERT INTO booking_agent_work_for (email, airline_name) VALUES (%s, %s)"
-                cur.execute(query, (form.email.data, form.airline_name.data))
-                conn.commit()
-                flash('Booking agent added successfully to the airline!', 'success')
-            else:
-                flash('You can only add agents to your own airline.', 'danger')
-                
         except Exception as e:
             flash(f'Failed to add booking agent. Error: {str(e)}', 'danger')
+
         finally:
             cur.close()
     return render_template('staff_add_agent.html', form=form)
@@ -230,18 +258,18 @@ def staff_operator():
         cur = conn.cursor()
 
         try:
-            # First, check if the flight number exists in the airline the staff belongs to
+            #check if the flight number exists in the airline the staff belongs to
             cur.execute("SELECT * FROM flight WHERE flight_num = %s AND airline_name = %s", (form.flight_num.data, session['airline']))
             flight = cur.fetchone()
             
             if flight:
-                # Update the flight status
+                # Update  flight status
                 query = "UPDATE flight SET status = %s WHERE flight_num = %s"
                 cur.execute(query, (form.status.data, form.flight_num.data))
                 conn.commit()
                 flash('Flight status updated successfully!', 'success')
             else:
-                # Flight does not exist under this operator's airline
+                # Not under same airline
                 flash('Flight number does not exist in your airline.', 'danger')
                 
         except Exception as e:
@@ -254,14 +282,56 @@ def staff_operator():
 
     return render_template('staff_operator.html', form=form)
 
-###Create seperate endpoints for these
+### Endpoints for staffs in general:
 
 # View all the booking agents: Top 5 booking agents based on number of tickets sales for the past month and
 # past year. Top 5 booking agents based on the amount of commission received for the last year.
-@staff.route('/view_booking_agents')
+@staff.route('/staff_view_booking_agents')
 def view_booking_agents():
+    conn = setup_db()
+    cur = conn.cursor(dictionary=True)
 
-    return render_template('view_booking_agents.html')
+    def get_top_agents(metric, period, metric_name="num_tickets"):
+        interval = "1 MONTH" if period == 'month' else "1 YEAR"
+        column = "COUNT(*)" if metric == 'tickets' else "SUM(flight.price)"  # Ensure price is from the flight table
+        query = f"""
+            SELECT booking_agent_id, {column} AS {metric_name}
+            FROM purchases
+            JOIN ticket ON purchases.ticket_id = ticket.ticket_id
+            JOIN flight ON ticket.flight_num = flight.flight_num  # Join flight table to access the price
+            WHERE airline_name = %s AND purchase_date >= DATE_SUB(CURDATE(), INTERVAL {interval})
+              AND booking_agent_id IS NOT NULL
+            GROUP BY booking_agent_id
+            ORDER BY {metric_name} DESC
+            LIMIT 5
+        """
+        cur.execute(query, (session['airline'],))
+        return cur.fetchall()
+
+    top_5_agents_past_month = get_top_agents('tickets', 'month')
+    top_5_agents_past_year = get_top_agents('tickets', 'year')
+    top_5_agents_commission_past_year = get_top_agents('commission', 'year', "commission")
+
+    # Get all booking agents in this airline
+    cur.execute("""
+        SELECT booking_agent_id, email
+        FROM booking_agent
+        JOIN booking_agent_work_for ON booking_agent.booking_agent_id = booking_agent_work_for.booking_agent_id
+        WHERE airline_name = %s
+    """, (session['airline'],))
+    booking_agents = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        'staff_view_agents.html',
+        top_5_agents_past_month=top_5_agents_past_month,
+        top_5_agents_past_year=top_5_agents_past_year,
+        top_5_agents_commission_past_year=top_5_agents_commission_past_year,
+        booking_agents=booking_agents
+    )
+
 
 
 # View frequent customers: Airline Staff will also be able to see the most frequent customer within the last
